27e9d31e261de6840e096086808f61868424b7f7
Tamim <farman.arefin1@outlook.com>
2021-10-18 01:36:00 +0600

    Second Commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClh0e-QM0POqpf3SXb2u9NJr60gVR7KGk",
+    authDomain: "tamim-diagonostic.firebaseapp.com",
+    projectId: "tamim-diagonostic",
+    storageBucket: "tamim-diagonostic.appspot.com",
+    messagingSenderId: "328531503009",
+    appId: "1:328531503009:web:9d98fa6ec642bec7b1cde0"
+};
+
+export default firebaseConfig;
