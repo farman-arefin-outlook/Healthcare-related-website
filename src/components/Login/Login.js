06486188c062c267f8e0205c3230d1b8e4888f68
Tamim <farman.arefin1@outlook.com>
2021-10-20 21:53:43 +0600

    Added logo

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,38 +1,60 @@
-import React from 'react';
-import { Button, Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Form, NavLink } from 'react-bootstrap';
 import useAuth from '../../hooks/useAuth';
+
+import google from './../../assets/images/google-logo.png';
+import facebook from './../../assets/images/facebook-logo.png';
+import github from './../../assets/images/github-logo.png';
 
 const Login = () => {
 
-    const { signInWithGoogle } = useAuth();
-    return (
-        <div><button onClick={signInWithGoogle}>Login With Google</button></div>
-        // <div className="my-4">
-        //     <h2 className="text-center mb-5">Please Login</h2>
-        //     <p className="text-danger text-center"></p>
-        //     <div className="w-25 mx-auto">
-        //         <Form>
-        //             <Form.Group className="mb-3" controlId="formBasicEmail">
-        //                 <Form.Label>Email address</Form.Label>
-        //                 <Form.Control type="email" placeholder="Enter email" />
-        //                 <Form.Text className="text-muted">
-        //                     We'll never share your email with anyone else.
-        //                 </Form.Text>
-        //             </Form.Group>
+    const { signInWithGoogle, signInWithGithub, signInWithFacebook, signInWithEmailandPass, error } = useAuth();
 
-        //             <Form.Group className="mb-3" controlId="formBasicPassword">
-        //                 <Form.Label>Password</Form.Label>
-        //                 <Form.Control type="password" placeholder="Password" />
-        //             </Form.Group>
-        //             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        //                 <Form.Check type="checkbox" label="Show Password" />
-        //             </Form.Group>
-        //             <Button variant="primary" type="submit">
-        //                 Log In
-        //             </Button>
-        //         </Form>
-        //     </div>
-        // </div>
+    return (
+        <div className="my-4">
+            <h2 className="text-center mb-5">Please Login</h2>
+            <p className="text-danger text-center"></p>
+            <div className="w-25 mx-auto">
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Show Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Sign In
+                    </Button>
+                </Form>
+            </div>
+            <p className="mt-2">
+                <NavLink className="text-decoration-none" to="/signup">
+                    Create an Account.
+                </NavLink>
+            </p>
+            <p className="mt-3">Or</p>
+            <p>Login With</p>
+            <div>
+                <button>
+                    <img src={google} width='50px' height='50px' borderRadius='50px' alt="google-icon" />
+                </button>
+                <button>
+                    <img src={facebook} width='50px' height='50px' borderRadius='50px' alt="facebook-icon" />
+                </button>
+                <button>
+                    <img src={github} width='50px' height='50px' borderRadius='50px' alt="github-icon" />
+                </button>
+            </div>
+        </div>
     );
 };
 
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,10 +1,12 @@
 import initializeAuthentication from "../Firebase/firebase.initialize";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, GithubAuthProvider, FacebookAuthProvider, signInWithEmailAndPassword, email, password } from "firebase/auth";
 import { useEffect, useState } from "react";
 initializeAuthentication();
 
 //Providers 
 const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 const auth = getAuth();
 const useFirebase = () => {
     const [user, setUser] = useState({});
@@ -22,6 +24,40 @@ const useFirebase = () => {
                 setError(error.message);
             });
     }
+    //For login with github
+    function signInWithGithub() {
+        signInWithPopup(auth, githubProvider)
+            .then((result) => {
+                setUser(result.user);
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    }
+
+    //for login with facebook 
+    const signInWithFacebook = () => {
+        signInWithPopup(auth, facebookProvider)
+            .then((result) => {
+                setUser(result.user);
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    }
+
+    //sign in with email and password
+
+    const signInWithEmailandPass = () => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                setUser(result.user);
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    }
+
     //get the currently sign-inned user 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -41,6 +77,9 @@ const useFirebase = () => {
             });
     }
     return {
+        signInWithEmailandPass,
+        signInWithFacebook,
+        signInWithGithub,
         logOut,
         signInWithGoogle,
         user,
